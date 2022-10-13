87e6f3c8afab9b67d1350d8f33bfcf6fc09348fb
williancordeiro <williancordeiro369@gmail.com>
2022-10-13 18:34:43 -0300

    comments

--- js/config.js
+++ js/config.js
@@ -1,9 +1,13 @@
 main();
 function main() {
+    
+    //Alerta de boas vindas
     alert("Bem-vindo(a) ao Letra à esquerda camarada!")
 
     recommendatio();
+    //cronometro();
 
+    //Função de Recomendação de Leitura
     function recommendatio() {
         let qtd = 0;
         qtd = prompt('Quantos livros você já leu?');
@@ -12,5 +16,19 @@ function main() {
             confirm('Uau! Aqui no LE você encontrará ainda mais conteúdo!');
         if (qtd == 0)
             confirm('É uma pena! Porém aqui no LE você vai encontrar divesos livros para ler!');
+    }
+
+    function cronometro() {
+        let segundos = 10;
+
+        setTimeout(decrement, 1000);
+
+        function decrement() {
+            segundos--;
+        }
+
+        document.querySelector("nav.le-navbar").innerHTML =
+            `<div>${segundos}</div>`
+
     }
 }
