c31837c9995f6d75ed274d56ec6823c25c861ad9
williancordeiro <williancordeiro369@gmail.com>
2022-12-04 15:10:56 -0300

    update

---
+++ app/register/js/config.js
@@ -0,0 +1,107 @@
+// ************************************************************************************ 
+
+// --------------------------- <| Script para pagina de registro |>  --------------------------- >
+
+// --------- <= Validação de campos com Regex => ---------
+
+let emailIsValid = (email) => {
+    let re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+function passwordIsValid(password) {
+    let re = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/;
+    return re.test(password);
+}
+
+let confirmPassword = (passwordConfirm)=> {
+    if (passwordConfirm === passwordR)
+        return true;
+}
+
+// --------- <= => ---------
+
+// ------------------ <= Dados para pagina de Registro => ------------------
+
+// ====== |Labels| ======
+
+let lbName = document.querySelector('#lbName');
+let lbLastName = document.querySelector('#lbLastName');
+let lbEmail = document.querySelector('#lbEmail');
+let lbPassword = document.querySelector('#lbPassword');
+let lbConfirmPassword = document.querySelector('#inpConfPassword');
+
+// ====== || ======
+
+// ====== |Inputs| ======
+
+let nameR = document.querySelector('#inpName');
+let lastNameR = document.querySelector('#inpLastName');
+let emailR = document.getElementById('inpEmail');
+let passwordR = document.querySelector('#inpPassword');
+let confirmPasswordR = document.querySelector('#inpConfPassword');
+
+// ====== || ======
+
+// ------------------ <= => ------------------
+
+// --------- <= Validação de campo HTML5 API => ---------
+(function validate() {
+    nameR.addEventListener('invalid', () => {
+        if (nameR.value == '')
+            nameR.setCustomValidity('Preencha com o seu Nome!');
+        else if (nameR.legth <= 2)
+            nameR.setCustomValidity('Nome precisa ter no minimo 3 Caracteres!');
+        else
+            nameR.setCustomValidity('');
+    });
+    
+    lastNameR.addEventListener('invalid', () => {
+        if (lastNameR.value == '')
+            lastNameR.setCustomValidity('Preencha com o seu Sobrenome!');
+        else
+            lastNameR.setCustomValidity('');
+    });
+    
+    emailR.addEventListener('invalid', () => {
+        if (emailR.value == '')
+            emailR.setCustomValidity('Preencha com email valido!');
+        else
+            emailR.setCustomValidity('');
+    });
+})();
+// --------- <= => ---------
+
+// --------- <= Verificação check-box => ---------
+
+(function optional() {
+    let optYes = document.querySelector('#optYes');
+    let opNo = document.querySelector('#optNo');
+
+    if (optYes.checked)
+        alert('Opção Sim');
+    if (opNo.checked)
+        alert('OPção Não');
+
+})();
+
+// --------- <= => ---------
+
+let reLogin = document.querySelector('p');
+let input = document.querySelector('input');
+
+function register() {
+    input.value = 'Salve';
+    if (emailIsValid(emailR.value) && passwordIsValid(passwordR.value)) {
+        alert('Sucesso');
+        setInterval(function() {
+            location.href = 'index.html';
+        }, 3000);
+    }
+
+    reLogin.innerHTML = 'Boa meu nobre! ';
+}
+
+// --------------------------- <| |> ---------------------------
+
+// ************************************************************************************
