089a0a6aabb8bb034b8584007baf1797be684f73
williancordeiro <williancordeiro369@gmail.com>
2022-12-08 17:03:47 -0300

    pspspsp

--- app/util/js/config.js
+++ app/util/js/config.js
@@ -1,5 +1,30 @@
 $(function() {
-    $('#buttonBook').click(function(e) {
+    $('#surprise').click(function(e) {
         e.preventDefault();
-    });
+
+        setInterval(function() {
+            $('nav').css('background-color', 'blue');
+        }, 1000)
+    })
 });
+
+$(function() {
+    $('#surprise').click(function(e) {
+        e.preventDefault();
+
+        setInterval(function() {
+            $('nav').css('background-color', 'green');
+        }, 2000)
+    })
+});
+
+$(function() {
+    $('#surprise').click(function(e) {
+        e.preventDefault();
+
+        setInterval(function() {
+            $('nav').css('background-color', 'yellow');
+        }, 3000)
+    })
+});
+
